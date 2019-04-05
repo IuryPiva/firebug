5f9d68bd7b0dd0b392f63da42867044b59a049ce
Iury Dias Piva <iurypiva@gmail.com>
2019-04-05 19:59:58 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,15 @@
+firebug
+=======
+
+Inspect firebase functions 
+
+[![oclif](https://img.shields.io/badge/cli-oclif-brightgreen.svg)](https://oclif.io)
+[![Version](https://img.shields.io/npm/v/firebug.svg)](https://npmjs.org/package/firebug)
+[![Downloads/week](https://img.shields.io/npm/dw/firebug.svg)](https://npmjs.org/package/firebug)
+[![License](https://img.shields.io/npm/l/firebug.svg)](https://github.com/IuryPiva/firebug/blob/master/package.json)
+
+<!-- toc -->
+# Usage
+<!-- usage -->
+# Commands
+<!-- commands -->
---
+++ src/index.js
@@ -0,0 +1,71 @@
+const { Command, flags } = require("@oclif/command");
+const execa = require("execa");
+const fs = require('fs');
+const camelCase = require('camelcase')
+const path = require('path')
+
+function generateFunctionName(file) { 
+  return camelCase(
+    path.basename(path.dirname(file))
+    .concat(
+      '/',
+      path.basename(
+        file, 
+        '.f'.concat(path.extname(file))
+      )
+    ).replace('/', '_')
+  )
+}
+
+class FirebugCommand extends Command {
+  async run() {
+    const { args } = this.parse(FirebugCommand);
+    const { functionName, dir } = args;
+    this.log(functionName);
+
+    switch (functionName) {
+      case "start":
+        this.exec("npx functions start");
+        break;
+      case "stop":
+        this.exec("npx functions stop");
+        break;
+      case "watch":
+        fs.watch(dir, { recursive: true } ,(eventType, filename) => {
+          this.log(`Detected ${eventType} on ${filename}`);
+          if(eventType == 'change' && filename.includes('.f.ts')) {
+            await this.firebug(generateFunctionName(filename))
+          }
+        })
+        break;
+      default:
+        this.firebug(functionName)
+    }
+  }
+
+  async exec(command) {
+    this.log(command);
+    const { stdout } = await execa.shell(command);
+    this.log(stdout);
+  }
+
+  async firebug(functionName) {
+    await this.exec(`npx functions deploy ${functionName} --trigger-http -t 10000s`);
+    await this.exec(`npx functions inspect ${functionName}`);
+  }
+}
+FirebugCommand.args = [{ name: "functionName" }, { name: "dir" }];
+
+FirebugCommand.description = `Inspect firebase functions that run locally
+...
+Extra documentation goes here
+`;
+
+FirebugCommand.flags = {
+  // add --version flag to show CLI version
+  version: flags.version({ char: "v" }),
+  // add --help flag to show CLI version
+  help: flags.help({ char: "h" })
+};
+
+module.exports = FirebugCommand;
